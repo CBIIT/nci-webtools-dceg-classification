7a3845194022aec5f6f26c3c79e8c2bdbcdffcf5
Brian Park <park-brian@users.noreply.github.com>
2018-10-16 15:27:03 -0400

    NCIATWP-1246: update static assets

--- soccer_python/app/static/js/soccer.js
+++ soccer_python/app/static/js/soccer.js
@@ -1,52 +1,93 @@
-// disables an element and updates its aria-disabled attribute
+/**
+ * Disables an element and updates its aria-disabled attribute
+ */
 $.fn.disable = function() {
-    $(this).prop('disabled', true).attr('aria-disabled', true);
+    return $(this)
+        .prop('disabled', true)
+        .attr('aria-disabled', true);
 }
 
-// enables an element and updates its aria-disabled attribute
+/**
+ * Enables an element and updates its aria-disabled attribute
+ */
 $.fn.enable = function() {
-    $(this).prop('disabled', false).attr('aria-disabled', false);
+    return $(this)
+        .prop('disabled', false)
+        .attr('aria-disabled', false);
+}
+
+/**
+ * Sets a progress indicator's text, aria-value, and width
+ * @param {number} value The numeric value of the indicator (0-100)
+ */
+$.fn.setProgress = function(value) {
+    return $(this)
+        .text(value + '%')
+        .attr('aria-value-now', value)
+        .css('width', value + '%');
+}
+
+/**
+ * Shows a bootstrap alert in the specified container
+ * @param {string} type The type of alert (bootstrap class name)
+ * @param {string} message The message to display within the alert
+ */
+$.fn.showAlert = function(type, message) {
+    return $(this).append(
+        $('<div class="alert" role="alert"/>')
+            .addClass(type)
+            .html(message)
+    );
+}
+
+/**
+ * Creates a FormData object from a form (which includes disabled elements)
+ */
+$.fn.formData = function() {
+    var formData = new FormData();
+    $(this).find('input, textarea, select').each(function(index, el) {
+        if (el.files) formData.append(el.name, el.files[0]);
+        else formData.append(el.name, el.value);
+    });
+    return formData;
 }
 
 $(function () {
 
-    reset();
-    // Resets the application (does not clear file input or model version)
-    function reset() {
+    // reset the soccer form
+    $('#soccer-form').on('reset', function() {
+        $('#input-file').val('').enable().change();
+        $('#model-version').val('1').enable();
+        $('#upload').enable().show();
+        $('#submit').enable().hide();
+        $('#reset').enable().show();
         $('#loading').hide();
-        $('#submit').enable();
-        $('#upload').enable();
-        $('#cancel').enable();
+
         $('#alerts').html('');
         $('#secondary-alerts').html('');
-        $('#email').val('');
-        $('#file-id').val('');
-        $('#email-container').hide();
-        $('#email').prop('required', false);
-        $('#submit-container').hide();
-        $('#results-container').hide();
-        $('#upload-progress').parent().hide();
-        setProgress(0, '#upload-progress');
-        return false;
-    }
 
-    // check if we have results
+        $('#file-id').val('');
+        $('#email').hide().val('').prop('required', false);
+
+        $('#results-container').hide();
+        $('#upload-progress').setProgress(0).parent().hide();
+        return false;
+    }).trigger('reset');
+
+    // disable default form submission
+    $('#soccer-form').submit(function (e) { return false });
+
+    // if we have a file id in the url, fetch and show the results
     var query = parseQueryString(location.search);
     if (query.id) {
         showResults(query.id);
         $('#soccer-tab').tab('show');
-    };
+    }
 
-    // Resets the form when the user clicks the cancel button
-    $('#cancel').click(function() {
-        $('#input-file').val('').change();
-        reset();
-    });
-
-    // Updates the input file's description when a new file is selected
+    // update the input file's description when a new file is selected
     $('#input-file').change(function (e) {
-        reset();
         $('#input-file-description').text('');
+        $('#upload-progress').setProgress(0);
         if (!this.files || !this.files.length) return;
 
         var file = this.files[0];
@@ -58,143 +99,125 @@ $(function () {
         $('#input-file-description').text(description);
     });
 
-    // resets the application if the model version changes
-    $('#model-version').change(reset);
-
-
     /**
      * When the user uploads an input file, attempt to validate it on the server.
      * If successful, we receive an object containing the keys:
-     *   - estimated_runtime - the estimated time to run the model
-     *   - file_id - a file id used to run the model and to retrieve results
+     *   - estimated_runtime - the estimated runtime for the model
+     *   - file_id - the file id used to run the model and to retrieve results
+     *
+     * Otherwise, we obtain a list of validation errors which are displayed
+     * in '#secondary-alerts'
      */
     $('#upload').click(function (e) {
         // do not proceed if there are no input files
         if (!$('#input-file').val()) return;
 
-        // reset the form (excluding file inputs)
-        reset();
-
+        $('#alerts').html('');
+        $('#secondary-alerts').html('');
+        $('#upload').disable();
         $('#upload-progress')
             .addClass('progress-bar-animated')
             .parent().show();
 
         $.post({
             url: 'validate',
-            data: new FormData($('#soccer-form')[0]),
+            data: $('#soccer-form').formData(),
             processData: false,
             contentType: false,
             xhr: function() {
                 var xhr = $.ajaxSettings.xhr();
                 xhr.upload.addEventListener('progress', function(e) {
-                    setProgress(Math.floor(100 * e.loaded / e.total));
+                    $('#upload-progress').setProgress(
+                        Math.floor(100 * e.loaded / e.total)
+                    );
                 }, false);
                 return xhr;
             }
         }).done(function (response) {
+            $('#input-file').disable();
+            $('#model-version').disable();
+
             $('#file-id').val(response.file_id);
+            $('#upload').hide();
+            $('#submit').show();
 
-            showAlert('alert-success', 'Your file has been uploaded successfully.');
-
+            $('#alerts').showAlert('alert-success', 'Your file has been uploaded successfully.');
             if (response.estimated_runtime > 30) {
                 // if the calculation is estimated to take more than 30 seconds, then we should ask the user to enter their email
-                $('#email-container').show();
-                $('#email').prop('required', true);
-                showAlert('alert-success', 'Since it will likely take longer than 30 seconds to process your data, please provide your email address, and you will get an email notification once the processing is complete.');
+                $('#email').show().prop('required', true);
+                $('#alerts').showAlert('alert-success', 'Since it will likely take longer than 30 seconds to process your data, please provide your email address, and you will get an email notification once the processing is complete.');
             }
-
-            $('#submit-container').show();
-            $('#upload').disable();
         }).fail(function (error) {
+            $('#upload').enable();
+
             console.log(error);
 
-            // display validation errors
-            var alertText = $('<div/>')
-                .append($('<p class="font-weight-bold"/>')
-                    .text('Your file has been uploaded successfully but contains the following errors:')
-                ).append($('<ul>').append(
-                    error.responseJSON.trim().split('\n').map(function(line) {
-                        return $('<li>').text(line);
-                    })
-                )).append($('<p class="font-weight-bold"/>')
-                    .text('Please modify your data file and re-upload.'));
+            // parse the list of validation errors
+            var errorList = $('<ul>').append(
+                error.responseJSON.trim().split(/\r?\n/).map(function(line) {
+                    return $('<li>').text(line);
+                })
+            );
 
-            showAlert('alert-warning', alertText, '#secondary-alerts');
+            // display validation errors
+            $('#secondary-alerts').showAlert('alert-warning',
+                $('<div/>')
+                    .append('<p><b>Your file has been uploaded successfully but contains the following errors:</b></p>')
+                    .append(errorList)
+                    .append('<p><b>Please modify your data file and re-upload.</b></p>')
+            );
         }).always(function() {
             $('#upload-progress').removeClass('progress-bar-animated');
         });
     });
 
     $('#submit').click(function(e) {
-        var form = $('#soccer-form')[0];
 
         // do not proceed if the form is invalid
-        if (!form.checkValidity()) return;
+        if (!$('#soccer-form')[0].checkValidity()) return;
 
         // determine if we should use 'enqueue' or 'code-file'
         var action = $('#email').val() ? 'enqueue' : 'code-file';
 
-        $('#submit-container').hide();
+        $('#submit').hide();
         $('#loading').show();
+        $('#reset').disable();
 
         $.post({
             url: action,
-            data: new FormData(form),
+            data: $('#soccer-form').formData(),
             processData: false,
             contentType: false,
         }).done(function(response) {
-            showAlert('alert-success', 'Your file has been processed successfully.');
-            showResults(response);
+            if (action === 'code-file') {
+                $('#alerts').showAlert('alert-success', 'Your file has been processed successfully.');
+                showResults(response);
+            } else {
+                $('#alerts').showAlert('alert-success', 'Your file has been enqueued successfully.');
+            }
         }).fail(function(error) {
             console.log(error);
-            showAlert('alert-warning', 'Processing has failed due to an unspecified error. Please contact <a href="mailto:NCISOCcerWebAdmin@mail.nih.gov">NCISOCcerWebAdmin@mail.nih.gov</a> if this problem persists.');
+            $('#alerts').showAlert('alert-warning', 'Processing has failed due to an internal error. Please contact <a href="mailto:NCISOCcerWebAdmin@mail.nih.gov">NCI&shy;SOCcer&shy;Web&shy;Admin@mail.nih.gov</a> if this problem persists.');
         }).always(function() {
+            $('#reset').enable();
             $('#loading').hide();
         });
     })
 
-    // disable default form submission when any button is clicked
-    $('#soccer-form').submit(function (e) { return false });
-
     /**
-     * Shows calculation results (sets attributes for graph/download link)
+     * Shows calculation results (sets attributes for plot/download link)
      * @param {string} id A file id containing the results
      */
     function showResults(id) {
         $('#results-container').hide();
         $.getJSON('results/' + id).done(function (results) {
-            $('#results-container').show();
             $('#plot').attr('src', results.plot_url);
             $('#download-link').attr('href', results.output_url);
+            $('#results-container').show();
+        }).fail(function(error) {
+            console.log(error);
+            $('#secondary-alerts').showAlert('alert-warning', 'We were unable to retrieve results for the specified file id.');
         });
-    }
-
-    /**
-     * Shows a bootstrap alert within the specified container (default: #alerts)
-     *
-     * @param {string} type The type of alert (bootstrap class name)
-     * @param {string} message The message to display within the alert
-     * @param {string | Node} container The alert's container
-     */
-    function showAlert(type, message, container) {
-        $(container || '#alerts').append(
-            $('<div class="alert"/>')
-                .css('white-space', 'pre-line')
-                .addClass(type)
-                .html(message)
-        );
-    }
-
-    /**
-     * Sets a progress indicator's text and width
-     * @param {number} value The numeric value of the indicator (0-100)
-     * @param {string | Node} container The progress indicator's selector
-     */
-    function setProgress(value, container) {
-        return $(container || '#upload-progress')
-            .text(value + '%')
-            .attr('aria-value-now', value)
-            .css('width', value + '%');
     }
 
     /**
@@ -225,7 +248,7 @@ $(function () {
     // enables bootstrap popovers
     $('[data-toggle="popover"]').popover();
 
-    // enables us to switch tabs by clicking on [data-tab] links
+    // allows us to switch tabs by clicking on [data-tab] links
     $('[data-tab]').click(function (e) {
         e.preventDefault();
         var tabSelector = $(this).attr('data-tab');
