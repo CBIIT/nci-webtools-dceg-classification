6e930bc0f6725fdc2c279f63f42b1c358f37ab08
Brian Park <park-brian@users.noreply.github.com>
2023-07-29 02:07:48 -0400

    implement client-side validation errors

--- client/soccer.js
+++ client/soccer.js
@@ -1,7 +1,6 @@
-form.onsubmit = submit;
-form.onreset = reset;
+form.onsubmit = handleSubmit;
+form.onreset = handleReset;
 form.onchange = handleChange;
-// form.file.onchange = validateFile;
 
 // load popovers
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
@@ -19,7 +18,7 @@ if (id) {
  * Submits the form
  * @param {Event} event
  */
-async function submit(event) {
+async function handleSubmit(event) {
   event.preventDefault();
   const body = new FormData(event.target);
 
@@ -53,7 +52,7 @@ async function submit(event) {
 /**
  * Resets the form and clears results and errorAlert
  */
-function reset() {
+function handleReset() {
   if (id) history.pushState(null, null, window.location.pathname);
   form.fieldset.disabled = false;
   form.submitButton.disabled = false;
@@ -74,14 +73,25 @@ function reset() {
  */
 async function handleChange() {
   const file = form.file.files && form.file.files[0];
+  const model = form.model.value;
 
-  // enforce background mode if file is larger than 4kb
   if (file) {
-    // todo: validate file
+    // validate file
+    let errors = await getValidationErrors(file, model);
+    if (errors.length) {
+      errorAlert.hidden = false;
+      errorMessage.innerText = errors.join("\n");
+      form.submitButton.disabled = true;
+    } else {
+      errorAlert.hidden = true;
+      errorMessage.innerText = "";
+      form.submitButton.disabled = false;
 
-    // if file is larger than 4kb, enforce background mode
-    if (file.size > 10000)
-      form.background.checked = true;
+      if (file.size > 10000) {
+        // if file is larger than 10kb, use background mode (this is also enforced on the server)
+        form.background.checked = true;
+      }
+    }
   }
 
   // disable email if background mode is disabled
@@ -120,9 +130,84 @@ async function loadResults(id) {
     // populate results
     resultsFile.href = resultsFileUrl;
     resultsPlot.src = resultsPlotUrl;
-    resultsPlot.onload = () => results.hidden = false;
+    resultsPlot.onload = () => (results.hidden = false);
   } catch (error) {
     errorAlert.hidden = false;
     errorMessage.innerText = error.message;
   }
+}
+
+/**
+ * Returns an array of validation errors for the specified file and model
+ * @param {File} file The file to validate
+ * @param {string} model The model version to validate against
+ * @returns {Promise<string[]>} An array of validation errors
+ */
+function getValidationErrors(file, model) {
+  const modelV1Headers = ["jobtitle", "sic", "jobtask"];
+  const modelV2Headers = ["id", "jobtitle", "sic", "jobtask"]; // v1 is backwards compatible with v2
+
+  return new Promise((resolve, reject) => {
+    let errors = [];
+    let line = 0;
+    let headers = [];
+    let hasValidHeaders = true;
+    let hasValidV1Headers = false;
+    let hasValidV2Headers = false;
+
+    Papa.parse(file, {
+      delimiter: ",",
+      error: reject,
+      complete: () => resolve(errors),
+      skipEmptyLines: true,
+      step: (results, parser) => {
+        line++; // use one-based indexing for users
+
+        // debugger;
+
+        // validate header
+        if (line === 1) {
+          headers = results.data.map((d) => d.toLowerCase());
+          hasValidV1Headers = JSON.stringify(headers) === JSON.stringify(modelV1Headers);
+          hasValidV2Headers = JSON.stringify(headers) === JSON.stringify(modelV2Headers);
+
+          if (model == "1.0" && !hasValidV1Headers) {
+            errors = errors.concat(`[Line ${line}] Invalid header, expected: ${modelV1Headers.join(",")}`);
+            hasValidHeaders = false;
+          } else if ((model === "1.9" || model === "2.0") && !hasValidV2Headers && !hasValidV1Headers) {
+            errors = errors.concat(`[Line ${line}] Invalid header, expected: ${modelV2Headers.join(",")} or ${modelV1Headers.join(",")}`);
+            hasValidHeaders = false;
+          }
+
+          if (!hasValidHeaders) {
+            parser.abort();
+          }
+
+          return;
+        }
+
+        // skip lines that contain only empty values
+        if (results.data.every((d) => d === "")) {
+          return;
+        }
+
+        // validate # of columns
+        if (results.data.length !== headers.length) {
+          errors = errors.concat(`[Line ${line}] Expected ${headers.length} columns, found ${results.data.length} columns`);
+        }
+
+        // validate sic
+        const sic = results.data[headers.indexOf("sic")];
+        if (sic && !/^\d+$/.test(sic)) {
+          errors = errors.concat(`[Line ${line}] Contains non-digit industry codes`);
+        }
+
+        // catch other errors
+        if (results.errors.length) {
+          let error = results.errors.map((r) => r.message).join(", ");
+          errors = errors.concat(`[Line ${line}] ${error}`);
+        }
+      },
+    });
+  });
 }
