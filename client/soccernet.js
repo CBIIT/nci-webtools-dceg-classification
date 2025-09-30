00329dfe69cece25ba252e908ef5d305f5be4fbc
danielruss <druss@mail.nih.gov>
2025-09-30 12:35:42 -0400

    Update website text + soccernet/clips form (#41)
    * html update, image update, switch update
    
    * update Help alerts
    
    * update text

--- client/soccernet.js
+++ client/soccernet.js
@@ -1,19 +1,32 @@
+import * as clips from "https://cdn.jsdelivr.net/npm/@danielruss/clips@latest/+esm";
 import * as soccerNet from "https://cdn.jsdelivr.net/npm/@danielruss/soccernet@latest/+esm"
 const outputSuffixMap = new Map([ ['CSV','csv'],['Excel','xlsx'],["JSONL","jsonl"]])
 
 const soccerModelSelect = document.getElementById("soccernet-model")
-const runFilePanel = document.getElementById("soccerNet-file");
-const runJobPanel = document.getElementById("soccerNet-singleJob");
-const runJobButton = document.getElementById("soccerNet-singleJob-run")
+const soccerNetFileSwitch = document.getElementById("fileSwitch")
+const soccerNetRunFilePanel = document.getElementById("soccerNet-file");
+const soccerNetRunJobPanel = document.getElementById("soccerNet-singleJob");
+
+const clipsRunJobPanel = document.getElementById("clips-singleJob");
 const outputElement = document.getElementById("soccerNet-singleJob-results");
-const fileElement = document.getElementById("soccerNet-file-file")
-const runFileButton = document.getElementById("soccerNet-file-run")
-let soccerConfig = await soccerNet.configureSOCcerNet();
-async function configSOCcerNet(){
-    console.log(soccerModelSelect.value)
-    soccerConfig = await soccerNet.configureSOCcerNet(soccerModelSelect.value)
+
+const soccerNetFileElement = document.getElementById("soccerNet-file-file")
+const soccerNetRunButton = document.getElementById("soccerNet-run-button")
+
+let modelConfig = await soccerNet.configureSOCcerNet();
+
+
+
+async function configApplication(){
+    // Am I running clips OR soccerNet?
+    let modelInfo = getModelInfo()
+    console.log("in configApplication: ",modelInfo)
+    if (modelInfo.name == "clips"){
+        modelConfig = await clips.configureClips(modelInfo.version)
+    } else {
+        modelConfig = await soccerNet.configureSOCcerNet(modelInfo.version)
+    }
 }
-soccerModelSelect.addEventListener("change",configSOCcerNet);
 
 
 
@@ -25,7 +38,8 @@ async function runSingleJob(event){
         console.log("... no input data...")
         return
     }
-    runJobButton.disabled = true;
+    soccerNetRunButton.disabled = true;
+
     let inputObject = {JobTitle: JobTitleElement.value,JobTask: JobTaskElement.value}
     const xw= ["soc1980","isco1988","noc2011"]
     xw.forEach( (cs) => {
@@ -39,30 +53,64 @@ async function runSingleJob(event){
         n=10
     }
 
-    let results = await soccerNet.runSOCcerPipeline(inputObject,soccerConfig,{n:n})
+    let results = await soccerNet.runSOCcerPipeline(inputObject,modelConfig,{n:n})
     buildResultsTable(results)
-    runJobButton.disabled=false;
+    clearSingleJobInput()
 }
+async function runSingleClipsJob(){
+    let products = document.getElementById("clips-singleJob-products").value.trim();
+    let sic1987 = document.getElementById("clips-singleJob-sic1987").value.trim();
+    let n = parseInt( document.getElementById("soccerNet-n").value )||10
+    // no products ... just return!
+    if (products.length == 0){
+        console.log("... no input data...")
+        return
+    }
+    soccerNetRunButton.disabled = true;
+    if (n<0 || n>689) {
+        n=10
+    }
+    let inputObject = {
+        products_services: products,
+        sic1987: sic1987
+    }
+    let results = await clips.runClipsPipeline(inputObject,modelConfig,{n:n})
+    buildResultsTable(results)
+    clearSingleJobInput()
+}
+
+
 
 function buildResultsTable(results){
     
     outputElement.classList.remove("d-none")
     outputElement.innerHTML = "";
+    const {name,_} = getModelInfo();
 
     let table = document.createElement("table");
     table.classList.add("table")
     let headerRow = table.insertRow();
-    headerRow.innerHTML = `<th>Rank</th><th>soc 2010</th><th>soc 2010 title</th><th>Score</th>`;
-    let n = results[0].soc2010.length;
+    headerRow.innerHTML = (name === "clips")
+        ? `<th>Rank</th><th>naics 2022</th><th>naics 2022 title</th><th>Score</th>`
+        : `<th>Rank</th><th>soc 2010</th><th>soc 2010 title</th><th>Score</th>`;
+    let n = results[0].score.length;
     for (let i=0; i<n; i++){
         let row = table.insertRow();
-        row.innerHTML = `<td>${i+1}</td><td>${results[0].soc2010[i]}</td><td>${results[0].title[i]}</td><td>${results[0].score[i].toFixed(4)}</td>`;
+        row.innerHTML = (name === "clips") 
+        ? `<td>${i+1}</td><td>${results[0].naics2022[i]}</td><td>${results[0].title[i]}</td><td>${results[0].score[i].toFixed(4)}</td>`
+        : `<td>${i+1}</td><td>${results[0].soc2010[i]}</td><td>${results[0].title[i]}</td><td>${results[0].score[i].toFixed(4)}</td>`;
     }
    outputElement.insertAdjacentElement("beforeend",table);
 }
 
-function clearSingleJob(){
+
+function clearSingleJobInput(){
     document.querySelectorAll('#soccerNet-singleJob [type="text"]').forEach((el)=>el.value="")
+    document.querySelectorAll('#clips-singleJob [type="text"]').forEach((el)=>el.value="")
+    soccerNetRunButton.disabled=false;
+}
+function clearSingleJob(){
+    clearSingleJobInput()
     outputElement.innerText=""
 }
 
@@ -94,13 +142,15 @@ function createAlert(message, type) {
     alertDiv.append(wrapper)
 }
 async function runFile(event){
-    
+    const {name,_} = getModelInfo()
+
     // no file given? return
-    if (fileElement.files.length<1){
+    if (soccerNetFileElement.files.length<1){
         return
     }
-    fileElement.disabled=true;
-    runFileButton.disabled=true;
+    soccerNetFileElement.disabled=true;
+    soccerNetRunButton.disabled=true;
+    soccerNetFileSwitch.disabled=true;
     setProgress(0);
 
     let n = parseInt( document.getElementById("soccerNet-n").value )||10;
@@ -109,7 +159,7 @@ async function runFile(event){
     }
 
     let outputType = document.getElementById("soccerNet-file-output-format").value;
-    let file = fileElement.files[0];
+    let file = soccerNetFileElement.files[0];
     let pb=document.getElementById("soccernet-file-progress")
 
     // break into blocks for efficent running
@@ -122,14 +172,18 @@ async function runFile(event){
     let writable = await soccerNet.createOPFSWritableStream(outputFilename)
     try {  
         block = await iterator.next()
-        let initial_metadata={};
+        let initial_metadata={
+            start_time: new Date().toLocaleString()
+        };
         let current_metadata={}
         while (!block.done){
-            let results = await soccerNet.runSOCcerPipeline(block.value,soccerConfig,{n:n})
+            let results = (name === "clips") ?
+                await clips.runClipsPipeline(block.value, modelConfig, { n: n }) :
+                await soccerNet.runSOCcerPipeline(block.value, modelConfig, { n: n });
             if(block.value.meta.blockId == 0){
-                initial_metadata = results.metadata
+                initial_metadata = await results.metadata
             }
-            current_metadata = results.metadata;
+            current_metadata = await results.metadata;
             soccerNet.writeResultsBlockToOPFS(results,writable,utf8Encoder,outputType)
 
             let nlines = block.value.meta.lines + block.value.data.length;
@@ -151,25 +205,88 @@ async function runFile(event){
 
 }
 function clearFile(){
-    fileElement.disabled=false;
-    fileElement.value=""
-    runFileButton.disabled=false;
+    soccerNetFileElement.value=""
+    soccerNetFileElement.disabled=false;
+    soccerNetRunButton.disabled=false;
+    soccerNetFileSwitch.disabled=false;
 }
 window.clearFile=clearFile
 
-document.getElementById("soccernet-file-switch").addEventListener("click",(event)=>{
-    // we could just toggle, but let's be safe
-    if (event.target.checked){
-        runFilePanel.classList.remove("d-none")
+
+
+function displayForm(event){
+    const modelInfo = getModelInfo();
+    const runFile = soccerNetFileSwitch.checked
+    console.log(modelInfo,runFile)
+
+    document.getElementById("soccernet-button-model").innerText = (modelInfo.name == "clips")?"CLIPS":"SOCcerNET"
+    clearSingleJob()
+    clearFile()
+
+    let label = document.querySelector("[for='soccernet-n']");
+    let n_element = document.getElementById("soccerNet-n");
+    let n = parseInt( n_element.value )||10;
+    if (runFile){
+        // The form is the same if you are running soccer or clips
+        soccerNetRunFilePanel.classList.remove("d-none")
         outputElement.classList.add("d-none")
-        runJobPanel.classList.add("d-none")
-        clearSingleJob()
-    }else{
-        runJobPanel.classList.remove("d-none")
+        soccerNetRunJobPanel.classList.add("d-none")
+        clipsRunJobPanel.classList.add("d-none");
+        if (modelInfo.name == "clips"){
+            n_element.max=689;
+            label.innerHTML = `# of codes requested<br><span id="n" class="fs-extra-small"> (max 689)</span>`;
+        } else {
+            n_element.max=840;
+            label.innerHTML = `# of codes requested<br><span id="n" class="fs-extra-small"> (max 840)</span>`;
+        }
+    } else if (modelInfo.name == "clips"){
+        // display the CLIPS single job input + output
+        clipsRunJobPanel.classList.remove("d-none");
+        document.getElementById("clips-singleJob-products").focus()
         outputElement.classList.remove("d-none")
-        runFilePanel.classList.add("d-none")
-        clearFile()
+        soccerNetRunJobPanel.classList.add("d-none")
+        soccerNetRunFilePanel.classList.add("d-none")
+        n_element.max=689;
+        label.innerHTML = `# of codes requested<br><span id="n" class="fs-extra-small"> (max 689)</span>`;
+    } else {
+        // display the SOCcer single job input
+        soccerNetRunJobPanel.classList.remove("d-none")
+        document.getElementById("soccerNet-singleJob-JobTitle").focus()
+        outputElement.classList.remove("d-none")
+        clipsRunJobPanel.classList.add("d-none");
+        soccerNetRunFilePanel.classList.add("d-none")
+        n_element.max=840;
+        label.innerHTML = `# of codes requested<br><span id="n" class="fs-extra-small"> (max 840)</span>`;
     }
-})
-runJobButton.addEventListener("click",runSingleJob)
-runFileButton.addEventListener("click",runFile)
+}
+function getModelInfo(){
+    return {
+        name: soccerModelSelect.selectedOptions[0].dataset.modelName,
+        version: soccerModelSelect.selectedOptions[0].dataset.modelVersion
+    }
+}
+function runSOCcer(){
+    let {name, _} = getModelInfo()
+    if (soccerNetFileSwitch.checked){
+        console.log("calling Runfile()")
+        runFile()
+    } else if (name == "clips"){
+        runSingleClipsJob()
+    }else{
+        runSingleJob()
+    }
+}
+
+
+
+soccerNetFileSwitch.addEventListener("click",displayForm)
+soccerModelSelect.addEventListener("change",configApplication);
+soccerModelSelect.addEventListener("change",displayForm);
+soccerNetRunButton.addEventListener("click",runSOCcer);
+// if the user clicks on the "single job" or "file" text span, toggle the switch
+function labelClicked(event){
+    soccerNetFileSwitch.checked = event.target.dataset.singleJob !== "true"
+    displayForm();
+}
+document.querySelectorAll('[data-single-job]').forEach((el)=>el.addEventListener("click",labelClicked));
+displayForm();
